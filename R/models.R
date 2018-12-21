73f1437e387305590c74373185b4fb09ce2db6ba
mitchell-thomann <mitchell.thomann@gmail.com>
2018-12-21 12:23:41 -0500

    linear model updated and documented - added editable prior distributions into BUGS text

--- R/models.R
+++ R/models.R
@@ -25,6 +25,7 @@ fitmod <- function(dat,
                    chains=1,
                    type = "bin",
                    trace_plots = FALSE) {
+  ## MT: add call to load model text in jags
   init_seed <- list(.RNG.seed = round(runif(1, 1, 10000)), .RNG.name = "base::Wichmann-Hill")
   model <- suppressWarnings(jags.model(textConnection(modtext), data = dat_jags, n.chains = chains,
                                        n.adapt = 1000, quiet = TRUE))
@@ -36,30 +37,41 @@ fitmod <- function(dat,
 
 #' Creates BUGS model for linear dose combination model
 #'
-#' @param data_type response data type, args = c("binomial")
+#' @param datatype character variable for response data type, args = c(\strong{"binomial"})
+#' @param prior list of parameters in model.  For this model, must contain lists: \strong{theta0}, 
+#'   \strong{theta1}, \strong{theta2}, \strong{theta12}.  Each list has values \emph{pmean} 
+#'   and \emph{psd} for prior means and standard deviations respectively.
+#'     
 #' @examples
-#' Dichotomous linear model:
-#' linear()
-#'
+#' Dichotomous linear combination model:
+#' linear("binomial", prior = list(theta0 = list(0,10), theta1 = list(0,10), 
+#'        theta2 = list(0,10), theta12 = list(0,10)))
+#'        
 #' @export
 #'
-linear <- function(data_type){
+#'
+linear <- function(datatype,
+                   prior = list(theta0=list(pmean,psd),
+                                theta1=list(pmean,psd),
+                                theta2=list(pmean,psd),
+                                theta12=list(pmean,psd))){
   if(data_type == "binomial"){
-    mod_text <- "
+    mod_text <- paste0("
     model{
       for(i in 1:N){
         y[i]~dbin(p[i],n[i])
         logit(p[i]) <- theta0+theta1*d1[i]+theta2*d2[i]+theta12*d1[i]*d2[i]
       }
-      theta0 ~ dnorm(mu0,1/sd0^2)
-      theta1 ~ dnorm(mu1,1/sd1^2)
-      theta2 ~ dnorm(mu2,1/sd2^2)
-      theta12 ~ dnorm(mu3,1/sd3^2)
+      theta0 ~ dnorm(",theta0$pmean,",1/",theta0$psd,"^2)
+      theta1 ~ dnorm(",theta1$pmean,",1/",theta1$psd,"^2)
+      theta2 ~ dnorm(",theta2$pmean,",1/",theta2$psd,"^2)
+      theta12 ~ dnorm(",theta12$pmean,",1/",theta12$psd,"^2)
       # probs
       for(i in 1:N){
         prob[i] <- ilogit(theta0+theta1*d1[i]+theta2*d2[i]+theta12*d1[i]*d2[i])
       }
     }
-    "
+    ")
   }
+  return(mod_text)
 }
